d6f3cd5e0148587cc4521aeddd7b47f0b75d8f9f
drdevinhopkins <devinhopkins@gmail.com>
2019-12-23 22:38:05 -0500

    3 models working

--- app.py
+++ app.py
@@ -126,26 +126,33 @@ def retrieve_future_data(api_key,location_list,frequency,num_of_days,location_la
 
 def main():
     st.title('JGH ED Visit Predictor')
-    model_selectbox = st.sidebar.selectbox('Prediction Model',('Daily', 'Hourly'))
+    model = st.sidebar.selectbox('Prediction Model',('2-week daily with weather', '72-hour hourly with weather','1-year daily'))
 
     my_placeholder = st.empty()
     my_placeholder.text("Loading model...")
 
-    if model_selectbox == 'Daily':
+    if model == '2-week daily with weather':
+        model = 'daily'
         pkl_path = "daily-19-12-22.pkl"
-    elif model_selectbox == 'Hourly':
+    elif model == '72-hour hourly with weather':
+        model = 'hourly'
         pkl_path = "hourly-19-12-22.pkl"
+    elif model == '1-year daily':
+        model = 'year'
+        pkl_path = "longterm-19-12-22.pkl"
 
     # read the Prophet model object
     with open(pkl_path, 'rb') as f:
         m = pickle.load(f)
 
-    if model_selectbox == 'Daily':
+    if model == 'daily':
         weather_forecast_on_file = pd.read_csv('Montreal-daily.csv')
-    elif model_selectbox == 'Hourly':
+    elif model == 'hourly':
         weather_forecast_on_file = pd.read_csv('Montreal-hourly.csv')
 
-    st.write("First weather date on file: "+weather_forecast_on_file.ds.min())
+    if model in ['daily','hourly']:
+        st.write("First weather date on file: "+weather_forecast_on_file.ds.min())
+
     now = datetime.now().astimezone(pytz.timezone('US/Eastern'))
     mtl_now = now.replace(tzinfo=None)
     local_now = datetime.now()
@@ -155,39 +162,49 @@ def main():
     #st.write('local_now: '+str(local_now))
     #st.write('mtl_now + 72 hours: '+str(mtl_now+timedelta(hours=72)))
 
-    if str(weather_forecast_on_file.ds.min()) == str(today):
-        st.write('Using weather forecast already on file')
-        weather_forecast = weather_forecast_on_file
-    else:
-        my_placeholder.text("Fetching the weather forecast...")
-        if model_selectbox == 'Daily':
-            frequency = 24
-            num_of_days = 15
-        elif model_selectbox == 'Hourly':
-            frequency = 1
-            num_of_days = 5
-        api_key = '3d51d04f983a478e90f164916191012'
-        location_list = ['Montreal']
+    if model in ['daily','hourly']:
+        if str(weather_forecast_on_file.ds.min()) == str(today):
+            st.write('Using weather forecast already on file')
+            weather_forecast = weather_forecast_on_file
+        else:
+            my_placeholder.text("Fetching the weather forecast...")
+            if model == 'daily':
+                frequency = 24
+                num_of_days = 15
+            elif model == 'hourly':
+                frequency = 1
+                num_of_days = 5
+            api_key = '3d51d04f983a478e90f164916191012'
+            location_list = ['Montreal']
 
-        retrieve_future_data(api_key,location_list,frequency,num_of_days)
+            retrieve_future_data(api_key,location_list,frequency,num_of_days)
 
-        if model_selectbox == 'Daily':
-            weather_forecast = pd.read_csv('Montreal-daily.csv')
-        elif model_selectbox == 'Hourly':
-            weather_forecast = pd.read_csv('Montreal-hourly.csv')
-        weather_forecast['ds']=pd.to_datetime(weather_forecast['ds'])
+            if model == 'daily':
+                weather_forecast = pd.read_csv('Montreal-daily.csv')
+            elif model == 'hourly':
+                weather_forecast = pd.read_csv('Montreal-hourly.csv')
+            weather_forecast['ds']=pd.to_datetime(weather_forecast['ds'])
 
     my_placeholder.text("Making predictions...")
     
-    if model_selectbox == 'Daily':
+    if model == 'daily':
         forecast = m.predict(weather_forecast)
-    elif model_selectbox == 'Hourly':
+    elif model == 'hourly':
         mask = (weather_forecast.ds >= mtl_now) & (weather_forecast.ds <= mtl_now+timedelta(hours=72))
         forecast = m.predict(weather_forecast.loc[mask])
+    elif model == 'year':
+        future = m.make_future_dataframe(365,'D')
+        #mask = (future.ds >= mtl_now)
+        #forecast = m.predict(future.loc[mask])
+        forecast = m.predict(future)
 
     st.header('Predictions')
 
-    x = [str(a) for a in forecast.ds.to_list()]
+    if model in ['daily','hourly']:
+        x = [str(a) for a in forecast.ds.to_list()]
+    elif model == 'year':
+        predictions = forecast[forecast.ds >= mtl_now]
+        x = [str(a) for a in predictions.ds.to_list()]
     y = forecast.yhat.to_list()
 
     fig = go.Figure(data=[go.Scatter(x=x, y=y)])
@@ -195,11 +212,17 @@ def main():
     st.plotly_chart(fig)
     #st.write(forecast[['ds', 'yhat']])
 
-    st.header('Weather Forecast')
-    if model_selectbox == 'Daily':
-        st.write(weather_forecast)
-    elif model_selectbox == 'Hourly':
-        st.write(weather_forecast.loc[mask])
+    if model in ['daily','hourly']:
+        st.header('Weather Forecast')
+        if model == 'daily':
+            st.write(weather_forecast)
+        elif model == 'hourly':
+            st.write(weather_forecast.loc[mask])
+
+    if model == 'year':
+        fig2 = m.plot_components(forecast)
+        st.plotly_chart(fig2)
+    
     my_placeholder.text("")
 
 
